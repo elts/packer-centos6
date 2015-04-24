e24da7325b7aae8e55d136ba511dc17942ec3ec2
Eldar Tsraev <elts@culab.org>
2015-04-24 16:17:13 +1200

    Initial commit

---
+++ provision.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ssh_key="ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key"
+
+echo > /etc/udev/rules.d/70-persistent-net.rules
+useradd -G wheel -m -p $(echo vagrant | openssl passwd -1 -stdin) -U vagrant
+mkdir -p ~vagrant/.ssh
+echo $ssh_key >> ~vagrant/.ssh/authorized_keys
+chown -R vagrant:vagrant ~vagrant/.ssh
+chmod 0700 ~vagrant/.ssh
+chmod 0600 ~vagrant/.ssh/authorized_keys
+echo "Defaults  !requiretty" > /etc/sudoers.d/vagrant
+echo "vagrant ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/vagrant
+yum update -y
